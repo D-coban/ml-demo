4fe0db5e5f6cdca779b176e1c5845a020d6b4d69
D-coban <fret.board.00006@gmail.com>
2023-12-19 22:38:21 +0100

    logging and exception handling

--- src/exception.py
+++ src/exception.py
@@ -1,5 +1,6 @@
 import sys
-import logging
+# import logging
+from src.logger import logging
 
 #if you want to understand the details about custom exceptions, like whats exec_info etc, read the documentation
 def error_message_details(error, error_details:sys):
